5efd2b8aac19c1ee60e7e7a3d098d95ac8962d83
free1990 <workmac@WorkMacdeiMac.local>
2014-12-12 16:35:34 +0800

    Add Getting a Method Address explain

--- TraningTarget-iOS/TraningTarget-iOS/RuntimeMsgForWarding.m
+++ TraningTarget-iOS/TraningTarget-iOS/RuntimeMsgForWarding.m
@@ -62,15 +62,18 @@
 //    for ( i = 0 ; i < 1000 ; i++ )
 //        setter(targetList[i], @selector(setFilled:), YES);
     
+    //msg 的发送都是通过isa指针然后去寻找selector ... address，如果本身找不到，就向上去寻找，直到找到为止
+    //现在有了另外的路径，直接通过使用methodForSelector去获得方法的指针，然后通过方法指针来调用方法，效率有很大的提高
+    //下面两个例子有阐述，一个是不到一秒一个是4秒多
     
-    //如果需要大量的调用方法可以通过直接获取方法的地址，然后进行调用Getting a Method Address
-    void (*setter)(id, SEL, BOOL);
-    int i;
-    
-    //10000次16:27:40.592-16:27:41.056
-    setter = (void (*)(id, SEL, BOOL))[self methodForSelector:@selector(test)];
-    for ( i = 0 ; i < 1000 ; i++ )
-        setter(self, @selector(test), YES);
+//    //如果需要大量的调用方法可以通过直接获取方法的地址，然后进行调用Getting a Method Address
+//    void (*setter)(id, SEL, BOOL);
+//    int i;
+//    
+//    //10000次16:27:40.592-16:27:41.056
+//    setter = (void (*)(id, SEL, BOOL))[self methodForSelector:@selector(test)];
+//    for ( i = 0 ; i < 1000 ; i++ )
+//        setter(self, @selector(test), YES);
     
 //    //10000次16:23:04.161   16:23:08.333
 //    for ( i = 0 ; i < 10000 ; i++ )

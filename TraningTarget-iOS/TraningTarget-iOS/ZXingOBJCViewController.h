1be7fa08da79a2af41d92e79f3ec1a4672849247
free1990 <workmac@WorkMacdeiMac.local>
2015-03-03 16:26:28 +0800

    add QR Code rec

---
+++ TraningTarget-iOS/TraningTarget-iOS/ZXingOBJCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZXingOBJCViewController.h
+//  TraningTarget-iOS
+//
+//  Created by John on 15/3/3.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ZXingOBJCViewController : BaseViewController
+
+@end
---
+++ TraningTarget-iOS/TraningTarget-iOS/ZXingOBJCViewController.m
@@ -0,0 +1,114 @@
+//
+//  ZXingOBJCViewController.m
+//  TraningTarget-iOS
+//
+//  Created by John on 15/3/3.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "ZXingOBJCViewController.h"
+#import "ZXingOBJCViewController.h"
+#import "ZXCGImageLuminanceSource.H"
+#import "ZXLuminanceSource.h"
+#import "ZXBinaryBitmap.h"
+#import "ZXHybridBinarizer.h"
+#import "ZXDecodeHints.h"
+#import "ZXMultiFormatReader.h"
+#import "ZXResult.h"
+
+@interface ZXingOBJCViewController (){
+    
+    UILabel *textLable;
+}
+
+@end
+
+@implementation ZXingOBJCViewController
+
++(void)load
+{
+    [super registerClassItem:self];
+}
+
+- (id)init{
+    if (self = [super init]) {
+        self.className   = @"ZXing";
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    textLable  = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+    
+    [textLable setTextColor:[UIColor blackColor]];
+    [textLable setCenter:self.view.center];
+    
+    [self.view addSubview:textLable];
+    
+    //容错30%
+    //大小280x280
+    
+    [self test];
+}
+
+- (void)test{
+    
+    UIImage *barcode = [UIImage imageNamed:@"barcode"];
+    
+    CGImageRef imageToDecode = barcode.CGImage;  // Given a CGImage in which we are looking for barcodes
+    
+    ZXLuminanceSource *source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
+    ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
+    
+    NSError *error = nil;
+    
+    // There are a number of hints we can give to the reader, including
+    // possible formats, allowed lengths, and the string encoding.
+    ZXDecodeHints *hints = [ZXDecodeHints hints];
+    
+    ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
+    ZXResult *result = [reader decode:bitmap
+                                hints:hints
+                                error:&error];
+    if (result) {
+        // The coded result as a string. The raw data can be accessed with
+        // result.rawBytes and result.length.
+        NSString *contents = result.text;
+        
+        NSLog(@"-------->  %@", contents);
+        
+        [textLable setText:[NSString stringWithFormat:@"识别结果: %@",result.text]];
+        // The barcode format, such as a QR code or UPC-A
+        ZXBarcodeFormat format = result.barcodeFormat;
+        
+        NSLog(@"ZXBarcodeFormat = %d", format);
+    } else {
+        
+        NSLog(@"error:%@", error);
+        
+        // Use error to determine why we didn't get a result, such as a barcode
+        // not being found, an invalid checksum, or a format inconsistency.
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

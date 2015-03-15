143ca3d1763df8e84d6cf9b0699d5d76dd7c8128
John <free1990@163.com>
2015-03-15 22:46:36 +0800

    begin learning pop

---
+++ TraningTarget-iOS/TraningTarget-iOS/PopLearnViewController.h
@@ -0,0 +1,13 @@
+//
+//  PopLearnViewController.h
+//  TraningTarget-iOS
+//
+//  Created by John on 15/3/15.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PopLearnViewController : BaseViewController
+
+@end
---
+++ TraningTarget-iOS/TraningTarget-iOS/PopLearnViewController.m
@@ -0,0 +1,49 @@
+//
+//  PopLearnViewController.m
+//  TraningTarget-iOS
+//
+//  Created by John on 15/3/15.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "PopLearnViewController.h"
+
+@interface PopLearnViewController ()
+
+@end
+
+@implementation PopLearnViewController
+
++(void)load
+{
+    [super registerClassItem:self];
+}
+
+- (id)init{
+    if (self = [super init]) {
+        self.className   = @"Pop Learn";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

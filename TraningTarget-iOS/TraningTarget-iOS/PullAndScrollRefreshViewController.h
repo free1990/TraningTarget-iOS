da0e6af28300735e9600ac921b8ec6487483f7ad
free1990 <free1990@163.com>
2015-03-23 09:21:09 +0800

    Add scroll refresh file.

---
+++ TraningTarget-iOS/TraningTarget-iOS/PullAndScrollRefreshViewController.h
@@ -0,0 +1,13 @@
+//
+//  PullAndScrollRefreshViewController.h
+//  TraningTarget-iOS
+//
+//  Created by John on 15/3/23.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PullAndScrollRefreshViewController : BaseViewController
+
+@end
---
+++ TraningTarget-iOS/TraningTarget-iOS/PullAndScrollRefreshViewController.m
@@ -0,0 +1,37 @@
+//
+//  PullAndScrollRefreshViewController.m
+//  TraningTarget-iOS
+//
+//  Created by John on 15/3/23.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "PullAndScrollRefreshViewController.h"
+
+@interface PullAndScrollRefreshViewController ()
+
+@end
+
+@implementation PullAndScrollRefreshViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

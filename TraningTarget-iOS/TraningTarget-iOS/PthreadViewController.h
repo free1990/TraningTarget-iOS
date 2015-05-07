26c784384af2f3cc29df67b0356c912ab96d2bfb
free1990 <free1990@163.com>
2015-05-07 14:56:21 +0800

    pthread

---
+++ TraningTarget-iOS/TraningTarget-iOS/PthreadViewController.h
@@ -0,0 +1,13 @@
+//
+//  PthreadViewController.h
+//  TraningTarget-iOS
+//
+//  Created by John on 15/5/7.
+//  Copyright (c) 2015年 WorkMac. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PthreadViewController : BaseViewController
+
+@end
